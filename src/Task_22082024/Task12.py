c3191e63f4967ee58ee45d4303193777bcd489ac
deeps-a09 <deepshikha.arya09@gmail.com>
2024-09-01 14:51:21 +0530

    Task 12 Commited and Pushed

---
+++ src/Task_22082024/Task12.py
@@ -0,0 +1,36 @@
+class Employee:
+    name = None
+    age = None
+    phone = None
+    address = None
+    eid = None
+
+    def __init__(self, name, age, phone, address, eid):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
+        self.eid = eid
+
+    def walk(self):
+        print("Employee " +self.name+ " is walking")
+        return None
+
+    def talk(self):
+        print("Employee " +self.name+ " is talking")
+        return None
+
+
+emp1 = Employee(input("Enter name: "), int(input("Enter age: ")), int(input("Enter phone: ")), input("Enter address: "),
+                input("Enter eid: "))
+print("Employee details: \n", "Name: ", emp1.name, "Age: ", emp1.age, "Phone: ", emp1.phone, "Address: ", emp1.address,
+      "EID: ", emp1.eid)
+emp1.walk()
+emp1.talk()
+
+emp2 = Employee(input("Enter name: "), int(input("Enter age: ")), int(input("Enter phone: ")), input("Enter address: "),
+                input("Enter eid: "))
+print("Employee details: \n", "Name: ", emp1.name, "Age: ", emp1.age, "Phone: ", emp1.phone, "Address: ", emp1.address,
+      "EID: ", emp1.eid)
+emp2.walk()
+emp2.talk()
